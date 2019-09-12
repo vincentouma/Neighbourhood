596c34755f6be79017fd0374582f58a7e1920a16
vincentouma <vinceoumah@gmail.com>
2019-09-12 22:48:49 +0300

    Create profile model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.db import IntegrityError
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='prof_pics/',blank=True)
+    prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    bio = models.TextField(default="")
+    location = models.CharField(max_length=200,blank=True)
+    contact_info = models.CharField(max_length=200,blank=True)
+    profile_Id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_location(self, location):
+        self. location = location
+        self.save()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()    
