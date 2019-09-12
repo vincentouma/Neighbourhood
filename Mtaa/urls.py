76b7abeee1a1cbc754b1583ee8d770d418a3d6e3
vincentouma <vinceoumah@gmail.com>
2019-09-12 22:47:56 +0300

    Add hood urls

--- Mtaa/urls.py
+++ Mtaa/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     url(r'',include('hood.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
-
-
+    
 ]
