306d8e8b9365fe858228e62df52dd934a9ff4144
vincentouma <vinceoumah@gmail.com>
2019-09-12 12:03:21 +0300

    Crate hopage view function

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+
+@login_required(login_url='/accounts/login/')
+def hood(request):
+
+
+    return render(request,'hood.html')
