0b2d7a1c902a1c4cef35244e295f5eeed3c81be9
vincentouma <vinceoumah@gmail.com>
2019-09-12 22:50:50 +0300

    Add profile urls

--- hood/urls.py
+++ hood/urls.py
@@ -8,5 +8,8 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^$',views.hood,name='hood'),
+    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
+    url(r'^edit/profile/$',views.profile_edit,name = 'edit_profile'),
 
 ]
