05d6efd29a6cd2d277b084bb429d9bd75ef7901d
vincentouma <vinceoumah@gmail.com>
2019-09-12 22:52:57 +0300

    Add profile form

---
+++ hood/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from.models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['prof_user','profile_Id']
+
+        
