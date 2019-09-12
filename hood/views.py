515b3a77da737912ce3563b24979b92c9cad2098
vincentouma <vinceoumah@gmail.com>
2019-09-12 22:52:02 +0300

    Crwate profile view function

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models.base import ObjectDoesNotExist
+from . forms import *
+from .models import *
 
 # Create your views here.
 
@@ -10,3 +15,42 @@ def hood(request):
 
 
     return render(request,'hood.html')
+
+
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/new_profile.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile/profile.html',{'profile':prof,})
