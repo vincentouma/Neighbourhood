953b787a2b34839015b65d2088431268efb07334
vincentouma <vinceoumah@gmail.com>
2019-09-12 12:05:09 +0300

    Add homepage url

---
+++ hood/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+
+urlpatterns = [
+    url(r'^$',views.hood,name='hood'),
+
+]
